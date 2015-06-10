9fe02cdc5f5892a2ac8bbbf30f812e3c8f7c8255
Shannon Barrett <sbarrett00@live.com>
2015-06-09 22:58:25 -0500

    Initial commit

---
+++ NR.Clone/Players/PlayerBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR.Clone.Players
+{
+    public class PlayerBase
+    {
+        public int CreditPool;
+        public string Identity;
+        public int ScoreCount;
+        public int ClickTracker;
+    }
+}
---
+++ NR.Clone/Players/Runner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NR.Clone.Cards
+
+namespace NR.Clone.Players
+{
+    class Runner : PlayerBase
+    {
+        public string[] Grip;
+        public Stack<Card> CardStack;
+        public Stack<Card> CardHeap;
+        public RunnerRig Rig;
+    }
+
+    public class RunnerRig
+    {
+        public Card[] ProgramRow;
+        public Card[] HardwareRow;
+        public Card[] ResourceRow;
+    }
+}
